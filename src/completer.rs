dc2835c56edc5da59cbbb6f4054053cf4163eb42
Marcelo Lima <marcelowind@gmail.com>
2019-10-27 20:58:35 +0100

    Use ureq

--- src/completer.rs
+++ src/completer.rs
@@ -21,45 +21,20 @@ fn parse_google(result: &String) {
 
 fn parse_duck(_result: &String) {}
 
-impl std::convert::From<curl::Error> for error::Error {
-    fn from(e: curl::Error) -> Self {
-        error::new("curl", e)
-    }
-}
-
-fn fetch(url: &String, query: &String) -> Result<String, error::Error> {
-    let mut buffer = Vec::new();
-    let mut curler = curl::easy::Easy::new();
-
-    {
-        let encoded_url = format!("{}{}", &url, &curler.url_encode(query.as_bytes()));
-        curler.url(encoded_url.as_str())?;
-        curler.get(true)?;
-        let mut transfer = curler.transfer();
-        transfer.write_function(|data| {
-            &buffer.extend_from_slice(data);
-            Ok(data.len())
-        })?;
-        transfer.perform()?;
-    }
-
-    match curler.response_code()? {
-        200 => Ok(()),
-        code => Err(error::new(
-            "completer::complete::fetch::curl",
-            format!("got response code {}", code),
-        )),
-    }?;
-
-    String::from_utf8(buffer).map_err(|e| error::new("completer::complete::fetch::parse", e))
-}
-
 pub fn complete(query: &String, url: &String, parser_name: &String) -> Result<(), error::Error> {
     if query.len() < 3 || url.is_empty() || parser_name.is_empty() {
         return Err(error::new("completer::complete", "Nothing to parse"));
     }
 
-    let result = fetch(&url, &query)?;
+    let result = {
+        let response = ureq::get(format!("{}{}", url, query).as_str()).call();
+
+        if response.ok() {
+            response.into_string().unwrap_or(String::new())
+        } else {
+            String::new()
+        }
+    };
 
     match parser_name.as_str() {
         "GOOGLE" => parse_google(&result),
