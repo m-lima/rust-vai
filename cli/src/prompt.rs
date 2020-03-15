60e75eefcd09997914b8096bddccf8fb9b597432
Marcelo Lima <marcelowind@gmail.com>
2020-03-15 18:21:18 +0100

    Testing some bundling

--- cli/src/prompt.rs
+++ cli/src/prompt.rs
@@ -1,3 +1,7 @@
 pub(super) fn run() -> ! {
-    std::process::exit(1);
+    std::thread::sleep(std::time::Duration::from_secs(10));
+    match std::fs::write(std::path::Path::new("/tmp/vai_output"), format!("{}", atty::is(atty::Stream::Stdout))) {
+        Ok(_) => std::process::exit(0),
+        Err(_) => std::process::exit(-1),
+    }
 }
